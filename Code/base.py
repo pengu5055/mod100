19263a22f73a7cf4491c480d1500b62ef7c754b6
Marko Urbanc <urbancmarko1@gmail.com>
2024-08-22 19:17:13 +0200

    feat: :sparkles: demo plot

--- Code/base.py
+++ Code/base.py
@@ -14,19 +14,21 @@ class Server:
                  server_bandwidth_in,
                  server_bandwidth_out,
                  ):
-        self.server_id = server_id
-        self.server_position = server_position
+        self.id = server_id
+        self.position = server_position
+        self.x = server_position[0]
+        self.y = server_position[1]
         self.server_bandwidth_in = server_bandwidth_in
         self.server_bandwidth_out = server_bandwidth_out
 
     def __str__(self):
-        return f"[Server: {self.server_id} // Bandwidth In: {self.server_bandwidth_in} // Bandwidth Out: {self.server_bandwidth_out}]"
+        return f"[Server: {self.id} // Bandwidth In: {self.server_bandwidth_in} // Bandwidth Out: {self.server_bandwidth_out}]"
     
     def __repr__(self):
-        return f"[Server: {self.server_id} // Bandwidth In: {self.server_bandwidth_in} // Bandwidth Out: {self.server_bandwidth_out}]"
+        return f"[Server: {self.id} // Bandwidth In: {self.server_bandwidth_in} // Bandwidth Out: {self.server_bandwidth_out}]"
     
     def __eq__(self, other):
-        return self.server_id == other.server_id and self.server_bandwidth_in == other.server_bandwidth_in and self.server_bandwidth_out == other.server_bandwidth_out
+        return self.id == other.server_id and self.server_bandwidth_in == other.server_bandwidth_in and self.server_bandwidth_out == other.server_bandwidth_out
     
     def __ne__(self, other):
         return not self.__eq__(other)
@@ -42,19 +44,21 @@ class User:
                  user_bandwidth_in,
                  user_bandwidth_out,
                  ):
-        self.user_id = user_id
-        self.user_position = user_position
+        self.id = user_id
+        self.position = user_position
+        self.x = user_position[0]
+        self.y = user_position[1]
         self.user_bandwidth_in = user_bandwidth_in
         self.user_bandwidth_out = user_bandwidth_out
 
     def __str__(self):
-        return f"[User: {self.user_id} // Bandwidth In: {self.user_bandwidth_in} // Bandwidth Out: {self.user_bandwidth_out}]"
+        return f"[User: {self.id} // Bandwidth In: {self.user_bandwidth_in} // Bandwidth Out: {self.user_bandwidth_out}]"
     
     def __repr__(self):
-        return f"[User: {self.user_id} // Bandwidth In: {self.user_bandwidth_in} // Bandwidth Out: {self.user_bandwidth_out}]"
+        return f"[User: {self.id} // Bandwidth In: {self.user_bandwidth_in} // Bandwidth Out: {self.user_bandwidth_out}]"
     
     def __eq__(self, other):
-        return self.user_id == other.user_id and self.user_bandwidth_in == other.user_bandwidth_in and self.user_bandwidth_out == other.user_bandwidth_out
+        return self.id == other.user_id and self.user_bandwidth_in == other.user_bandwidth_in and self.user_bandwidth_out == other.user_bandwidth_out
     
     def __ne__(self, other):
         return not self.__eq__(other)
@@ -99,10 +103,10 @@ class Grid:
                 self.grid[x][y] = Wire(x * size + y, (x, y), np.random.random())
 
     def add_server(self, server):
-        self.grid[server.server_position] = server
+        self.grid[server.position] = server
 
     def add_user(self, user):
-        self.grid[user.user_position] = user
+        self.grid[user.position] = user
     
     def plot_wires(self):
         output = np.zeros((self.size, self.size))
@@ -121,18 +125,18 @@ class Grid:
     
     def plot_servers(self):
         list_servers = [x for x in self.grid.flatten() if isinstance(x, Server)]
-        output = np.array([(x.server_id, x.server_position, x.server_bandwidth_in, x.server_bandwidth_out) for x in list_servers])
+        output = [[s.id, s.x, s.y, s.server_bandwidth_in, s.server_bandwidth_out] for s in list_servers]
 
-        return output
+        return np.array(output)
     
     def plot_users(self):
         list_users = [x for x in self.grid.flatten() if isinstance(x, User)]
-        output = np.array([(x.user_id, x.user_position, x.user_bandwidth_in, x.user_bandwidth_out) for x in list_users])
+        output = [[u.id, u.x, u.y, u.user_bandwidth_in, u.user_bandwidth_out] for u in list_users]
 
-        return output
+        return np.array(output)
     
     def __str__(self):
-        output = np.zeros((self.size, self.size), dtype=str)
+        output = np.zeros((self.size, self.size), dtype=object)
         for x in range(self.size):
             for y in range(self.size):
                 if isinstance(self.grid[x][y], Wire):
@@ -146,17 +150,18 @@ class Grid:
                         output[x][y] = "+"
 
                 elif isinstance(self.grid[x][y], Server):
-                    output[x][y] = f"S:{self.grid[x][y].server_id}"
+                    output[x][y] = f"S:{self.grid[x][y].id}"
+                    print(output[x][y])
                 elif isinstance(self.grid[x][y], User):
-                    output[x][y] = f"U:{self.grid[x][y].user_id}"
+                    output[x][y] = f"U:{self.grid[x][y].id}"
                 else:
                     raise ValueError(f"Invalid type {type(self.grid[x][y])} in grid.")
         
         # Fix corners 
-        output[0][0] = "┌"
-        output[0][self.size - 1] = "┐"
-        output[self.size - 1][0] = "└"
-        output[self.size - 1][self.size - 1] = "┘"
+        output[0][0] = "┌" if output[0][0] == "-" else output[0][0]
+        output[0][self.size - 1] = "┐" if output[0][self.size - 1] == "-" else output[0][self.size - 1]
+        output[self.size - 1][0] = "└" if output[self.size - 1][0] == "-" else output[self.size - 1][0]
+        output[self.size - 1][self.size - 1] = "┘" if output[self.size - 1][self.size - 1] == "-" else output[self.size - 1][self.size - 1]
 
         return "\n".join(["".join([str(x + "\t") for x in row]) for row in output])
     
--- Code/test.py
+++ Code/test.py
@@ -2,8 +2,57 @@
 Scratchpad to test implementations.
 """
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import cmasher as cmr
 from base import *
 
-grid = Grid(10)
+# Use Custom Style
+mpl.style.use("./ma-style.mplstyle")
+colors = cmr.take_cmap_colors("cmr.tropical", 8, cmap_range=(0, 0.85))
 
-print(grid)
+grid = Grid(10)
+server = Server(0, (5, 5), 0.5, 0.5)
+user = User(0, (0, 0), 0.5, 0.5)
+grid.add_server(server)
+grid.add_user(user)
+
+fig, ax = plt.subplots(figsize=(6, 5), layout="compressed")
+
+# Plot Wires
+wires = grid.plot_wires()
+cmap = mpl.colors.ListedColormap(colors)
+img = ax.imshow(wires, cmap=cmap, vmin=0, vmax=1, zorder=5,
+                extent=[0, grid.size, 0, grid.size])
+cbar = fig.colorbar(img, ax=ax, orientation="horizontal", pad=0.1)
+cbar.set_label("Bandwidth")
+cbar.set_ticks([0, 0.2, 0.4, 0.6, 0.8, 1])
+cbar.set_ticklabels(["0", "0.2", "0.4", "0.6", "0.8", "1"])
+
+# Plot Servers
+servers = grid.plot_servers()
+img = plt.imread("x.png")            
+for server in servers:
+    ax.imshow(img, extent=[server[1], server[1] + 1, server[2] - 1, server[2]], zorder=10)
+# Plot Users
+users = grid.plot_users()
+for user in users:
+    ax.text(user[1], user[2], f"U{user[0]}", ha="center", va="center", color="black")
+
+# Plot Grid
+if True:
+    for x in np.arange(0.5, grid.size - 0.5, 1):
+        for y in np.arange(0.5, grid.size - 0.5, 1):
+            ax.plot([x, x], [y, y + 1], color="black", alpha=0.1, lw=2, zorder=5)
+            ax.plot([x, x + 1], [y, y], color="black", alpha=0.1, lw=2, zorder=5)
+            ax.plot([x, x + 1], [y + 1, y + 1], color="black", alpha=0.1, lw=2, zorder=10)
+            ax.plot([x + 1, x + 1], [y, y + 1], color="black", alpha=0.1, lw=2, zorder=10)
+
+
+ax.set_xticks(np.arange(0.5, grid.size, 1))
+ax.set_xticklabels(np.arange(0, grid.size, 1))
+ax.set_yticks(np.arange(0.5, grid.size, 1))
+ax.set_yticklabels(np.flip(np.arange(0, grid.size, 1)))
+ax.grid(which="minor", color="black", linestyle="-", linewidth=2)
+
+plt.show()
