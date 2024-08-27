5fc758ffbd792c9bdf3b73d97f3f005f600faa64
Marko Urbanc <urbancmarko1@gmail.com>
2024-08-27 23:05:36 +0200

    feat: :sparkles: almost working

--- Code/one-flow-opt.py
+++ Code/one-flow-opt.py
@@ -11,14 +11,14 @@ import pulp
 # Use Custom Style
 mpl.style.use("./ma-style.mplstyle")
 colors = cmr.take_cmap_colors("cmr.tropical", 8, cmap_range=(0, 0.85))
-cm = custom_cmap
+cm = cmr.get_sub_cmap("cmr.nuclear", 0.3, 0.7)
 
 # Initiate Grid
 grid = Grid(10)
 indices = [(i, j) for i in range(grid.size) for j in range(grid.size)]
 for ind in indices:
     i, j = ind
-    grid[i][j].bandwidth = 1
+    grid[i][j].bandwidth = 0.5
 
 server = Server(0, (6, 6), 0.7, 0.7)
 grid.add_server(server)
@@ -29,36 +29,62 @@ grid.add_user(user)
 
 # Setup LP Problem
 lp_problem = pulp.LpProblem("Network_Flow", pulp.LpMaximize)
+four_flow_positive = {
+    "N": pulp.LpVariable.dicts("Flow_N_pos", indices, lowBound=0, upBound=1, cat="Continuous"),
+    "E": pulp.LpVariable.dicts("Flow_E_pos", indices, lowBound=0, upBound=1, cat="Continuous"),
+    "S": pulp.LpVariable.dicts("Flow_S_pos", indices, lowBound=0, upBound=1, cat="Continuous"),
+    "W": pulp.LpVariable.dicts("Flow_W_pos", indices, lowBound=0, upBound=1, cat="Continuous"),
+}
+four_flow_negative = {
+    "N": pulp.LpVariable.dicts("Flow_N_neg", indices, lowBound=0, upBound=1, cat="Continuous"),
+    "E": pulp.LpVariable.dicts("Flow_E_neg", indices, lowBound=0, upBound=1, cat="Continuous"),
+    "S": pulp.LpVariable.dicts("Flow_S_neg", indices, lowBound=0, upBound=1, cat="Continuous"),
+    "W": pulp.LpVariable.dicts("Flow_W_neg", indices, lowBound=0, upBound=1, cat="Continuous"),
+}
 four_flow = {
-    "N": pulp.LpVariable.dicts("Flow_N", indices, lowBound=0, upBound=1, cat="Continuous"),
-    "E": pulp.LpVariable.dicts("Flow_E", indices, lowBound=0, upBound=1, cat="Continuous"),
-    "S": pulp.LpVariable.dicts("Flow_S", indices, lowBound=0, upBound=1, cat="Continuous"),
-    "W": pulp.LpVariable.dicts("Flow_W", indices, lowBound=0, upBound=1, cat="Continuous"),
+    "N": pulp.LpVariable.dicts("Flow_N", indices, lowBound=-1, upBound=1, cat="Continuous"),
+    "E": pulp.LpVariable.dicts("Flow_E", indices, lowBound=-1, upBound=1, cat="Continuous"),
+    "S": pulp.LpVariable.dicts("Flow_S", indices, lowBound=-1, upBound=1, cat="Continuous"),
+    "W": pulp.LpVariable.dicts("Flow_W", indices, lowBound=-1, upBound=1, cat="Continuous"),
 }
 
 # Set Initial Values
 for i, j in indices:
     for dir in four_flow.keys():
-        four_flow[dir][(i, j)].setInitialValue(0)
+        four_flow_positive[dir][(i, j)].setInitialValue(0)
+        four_flow_negative[dir][(i, j)].setInitialValue(0)
+
+# Constraint 0: Build flow from positive and negative flow
+for i, j in indices:
+    for dir in four_flow.keys():
+        lp_problem += four_flow[dir][(i, j)] == four_flow_positive[dir][(i, j)] - four_flow_negative[dir][(i, j)], f"Flow_Build_{dir}_{i}_{j}"
 
 # Constraint 1: Flow must obey the bandwidth constraints
 for i, j in indices:
     if isinstance(grid[i][j], User):
-        lp_problem += pulp.lpSum([four_flow[dir][(i, j)] for dir in four_flow.keys()]) == grid[i][j].bandwidth
-    else:
-        lp_problem += pulp.lpSum([four_flow[dir][(i, j)] for dir in four_flow.keys()]) <= grid[i][j].bandwidth
+        lp_problem += pulp.lpSum([four_flow_positive[dir][(i, j)] for dir in four_flow.keys()]) == 0, f"User_Out_{i}_{j}"
+        lp_problem += pulp.lpSum([four_flow_negative[dir][(i, j)] for dir in four_flow.keys()]) == grid[i][j].bandwidth, f"User_{i}_{j}"
+    elif isinstance(grid[i][j], Server):
+        lp_problem += pulp.lpSum([four_flow_negative[dir][(i, j)] for dir in four_flow.keys()]) == 0, f"Server_In_{i}_{j}"
+        lp_problem += pulp.lpSum([four_flow_positive[dir][(i, j)] for dir in four_flow.keys()]) <= grid[i][j].bandwidth, f"Server_{i}_{j}"
+    elif isinstance(grid[i][j], Wire):
+        lp_problem += pulp.lpSum([four_flow_positive[dir][(i, j)] + four_flow_negative[dir][(i, j)] for dir in four_flow.keys()]) <= grid[i][j].bandwidth, f"Wire_{i}_{j}"
+        lp_problem += pulp.lpSum([four_flow[dir][(i, j)] for dir in four_flow.keys()]) == 0, f"Wire_Leak_Prevention_{i}_{j}"
 
 # Constraint 2: Continuity of Flow
 for i, j in indices:
     neighbors = grid.get_neighbors(i, j)
     for dir, neighbor in neighbors.items():
         if neighbor is not None:
-            lp_problem += four_flow[dir][(i, j)] == four_flow[grid.get_reverse_direction(dir)][neighbor], f"Flow_Continuity_{dir}_{i}_{j}"
+            lp_problem += four_flow_positive[dir][(i, j)] == four_flow_positive[grid.get_reverse_direction(dir)][neighbor], f"Flow_Continuity_P_{dir}_{i}_{j}"
+            lp_problem += four_flow_negative[dir][(i, j)] == four_flow_negative[grid.get_reverse_direction(dir)][neighbor], f"Flow_Continuity_N_{dir}_{i}_{j}"
         else:
             lp_problem += four_flow[dir][(i, j)] == 0, f"Flow_Continuity_{dir}_{i}_{j}"
         
 # Define Objective Function
-lp_problem += pulp.lpSum([four_flow[dir][(user[1], user[2])] for dir in four_flow.keys()] for user in grid.get_users())
+# lp_problem += pulp.lpSum([four_flow[dir][(user[1], user[2])] for dir in four_flow.keys()] for user in grid.get_users())
+lp_problem += pulp.lpSum([four_flow[dir][(x, y)] for dir in four_flow.keys() for x, y in indices if isinstance(grid[x][y], Server)]) \
+            - pulp.lpSum([four_flow[dir][(x, y)] for dir in four_flow.keys() for x, y in indices if isinstance(grid[x][y], Wire)])         
 
 # Solve LP Problem
 lp_problem.solve(pulp.GUROBI(msg=True, warmStart=True))
@@ -67,10 +93,12 @@ print("Status:", pulp.LpStatus[lp_problem.status])
 # Extract Results
 flow_results = {dir: np.zeros((grid.size, grid.size)) for dir in four_flow.keys()}
 flow_sum = np.zeros((grid.size, grid.size))
+flow_abs_sum = np.zeros((grid.size, grid.size))
 for i, j in indices:
     for dir in four_flow.keys():
         flow_results[dir][i][j] = four_flow[dir][(i, j)].value()
         flow_sum[i][j] += flow_results[dir][i][j]
+        flow_abs_sum[i][j] += four_flow_positive[dir][(i, j)].value() + four_flow_negative[dir][(i, j)].value()
 
 max_flow = np.max([flow_results[dir].max() for dir in four_flow.keys()])
 if max_flow == 0:
@@ -80,15 +108,12 @@ else:
     print(f"Max Flow: {max_flow}")
 
 fig, ax = plt.subplots(1, 1, figsize=(8, 8), layout="compressed")
-# for i, j in indices:
-#     for dir in four_flow.keys():
-#         if flow_results[dir][i][j] > 0:
-#             ax.arrow(j, i, grid.get_dir_offset(dir)[1], grid.get_dir_offset(dir)[0], head_width=0.1, head_length=0.1, fc=colors[0], ec=colors[0])
-cm2 = cmr.get_sub_cmap("cmr.redshift", 0.15, 0.85)
-norm = mpl.colors.Normalize(vmin=0, vmax=max_flow)
+ax.set_title("Network Flow Optimization")
+cm2 = cmr.get_sub_cmap("cmr.redshift", 0.2, 0.8)
+norm = mpl.colors.Normalize(vmin=-1, vmax=1)
 sm = plt.cm.ScalarMappable(cmap=cm2, norm=norm)
 
-img = ax.imshow(flow_sum.T, cmap=cm2, norm=norm, zorder=5, origin="lower", aspect="auto",
+img = ax.imshow(flow_abs_sum.T, cmap=cm2, norm=norm, zorder=5, origin="lower", aspect="auto",
                 extent=[0, grid.size, 0, grid.size], alpha=1)
 cbar = fig.colorbar(sm, ax=ax, orientation="vertical", fraction=0.046, pad=0.04)
 cbar.set_label("Flow")
@@ -117,20 +142,20 @@ for i, j in indices:
             path.append((i, j, flow_results[direction][i, j], direction))
 path = sorted(path, key=lambda x: x[2], reverse=True)
 path = np.array(path)
-print(path)
 
 norm = mpl.colors.Normalize(vmin=0, vmax=max_flow)
 sm = plt.cm.ScalarMappable(cmap=cm, norm=norm)
-cbar2 = fig.colorbar(sm, ax=ax, orientation="horizontal")
+cbar2 = fig.colorbar(sm, ax=ax, orientation="horizontal", fraction=0.046, pad=0.04)
 cbar2.set_label("Flow")
 
 for i, j, flow, _ in path:
     i, j = int(i) + 0.5, int(j) + 0.5
     flow = float(flow)
+    print(flow)
     for dir in four_flow.keys():
         if flow_results[dir][int(i - 0.5), int(j - 0.5)] != 0:
             i_next, j_next = i + FLOW_INDEX[dir][0], j + FLOW_INDEX[dir][1]
-            ax.plot([i, i_next], [j, j_next], color=cm(norm(flow)), lw=2, zorder=8)
+            ax.plot([i, i_next], [j, j_next], color=cm(norm(flow)), lw=5, zorder=8)
 
 # Compass
 if False:
