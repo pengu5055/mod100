473e7eebccc06de7f81d418347f98eb499a1d3a2
Marko Urbanc <urbancmarko1@gmail.com>
2024-08-30 22:16:46 +0200

    feat: :art: add histogram

--- Code/plot-bandwidth.py
+++ Code/plot-bandwidth.py
@@ -15,28 +15,57 @@ mpl.style.use("./ma-style.mplstyle")
 colors = cmr.take_cmap_colors("cmr.tropical", 8, cmap_range=(0, 0.85))
 
 # Load Data
-server_bandwidths = np.load("./Data/server_bandwidths-5-10-lowuser.npy")
+s_bw = np.load("./Data/server_bandwidths-5-10-lowuser.npy")
+s_bw2 = np.load("./Data/server_bandwidths-10-0.1.npy")
+s_bw3 = np.load("./Data/server_bandwidths-10-2.npy")
+s_bw4 = np.load("./Data/server_bandwidths-10-5.npy")
 
+bw_1 = len(s_bw)/2400
+bw_2 = len(s_bw2)/2400
+bw_3 = len(s_bw3)/2400
+bw_4 = len(s_bw4)/2400
 
-fig, ax = plt.subplots(1, 1, figsize=(12, 8), layout="compressed")
+fig, ax = plt.subplots(2, 2, figsize=(12, 8), layout="compressed")
+ax = ax.flatten()
 
-n, bins = np.histogram(server_bandwidths, bins=15)
+n, bins = np.histogram(s_bw, bins=15, density=False)
 colors = cmr.take_cmap_colors("cmr.tropical", len(bins), cmap_range=(0, 0.85))
-ax.bar(bins[:-1], n, width=np.diff(bins), color=colors, edgecolor="black", linewidth=0.5, zorder=4)
-# Fit Normal Distribution
-print(np.array(n).max())
-bins = bins + (bins[1]-bins[0])/2
-n_total = np.sum(n, axis=0)/np.sum(n)
-n_total = n_total/np.max(n_total)
-print(n_total)
+ax[0].bar(bins[:-1], n, width=np.diff(bins), color=colors, edgecolor="black", linewidth=0.5, zorder=4, label=f"Server Bandwidth\nSamples: {len(s_bw)}")
+ax[0].bar(bins[:-1], n/(bw_1), width=np.diff(bins), color=colors, edgecolor="black", linewidth=0.5, zorder=3, alpha=0.2, label=f"Adjusted for 2400\nServer Samples")
+ax[0].set_title("Beta(5, 10) Low User Demand of 0.1")
+ax[0].set_xlabel("Bandwidth")
+ax[0].set_ylabel("Frequency")
+ax[0].set_xticks(np.arange(0, 1.1, 0.1))
 
-popt, pcov = curve_fit(gaussian, bins[:-1], n_total, p0=[0.7, 0.5])
-x = np.linspace(0, 0.2, 100)
-y = gaussian(x, *popt)
-print(np.max(n))
-y = y/np.max(y) * np.mean(n, axis=0).max()
-ax.plot(x, y, color="black", ls="--", linewidth=2, label="Normal Distribution", zorder=5)
-ax.legend()
-ax.set_title("Server Bandwidth Distribution")
-ax.set_xlabel("Bandwidth")
+n, bins = np.histogram(s_bw2, bins=15, density=False)
+colors = cmr.take_cmap_colors("cmr.tropical", len(bins), cmap_range=(0, 0.85))
+ax[1].bar(bins[:-1], n, width=np.diff(bins), color=colors, edgecolor="black", linewidth=0.5, zorder=4, label=f"Server Bandwidth\nSamples: {len(s_bw2)}")
+ax[1].bar(bins[:-1], n/(bw_2), width=np.diff(bins), color=colors, edgecolor="black", linewidth=0.5, zorder=3, alpha=0.2, label=f"Adjusted for 2400\nServer Samples")
+ax[1].set_title("Beta(10, 0.1)")
+ax[1].set_xlabel("Bandwidth")
+ax[1].set_ylabel("Frequency")
+ax[1].set_xticks(np.arange(0, 1.1, 0.1))
+
+n, bins = np.histogram(s_bw3, bins=15, density=False)
+colors = cmr.take_cmap_colors("cmr.tropical", len(bins), cmap_range=(0, 0.85))
+ax[2].bar(bins[:-1], n, width=np.diff(bins), color=colors, edgecolor="black", linewidth=0.5, zorder=4, label=f"Server Bandwidth\nSamples: {len(s_bw3)}")
+ax[2].bar(bins[:-1], n/(bw_3), width=np.diff(bins), color=colors, edgecolor="black", linewidth=0.5, zorder=3, alpha=0.2, label=f"Adjusted for 2400\nServer Samples")
+ax[2].set_title("Beta(10, 2)")
+ax[2].set_xlabel("Bandwidth")
+ax[2].set_ylabel("Frequency")
+ax[2].set_xticks(np.arange(0, 1.1, 0.1))
+
+n, bins = np.histogram(s_bw4, bins=15, density=False)
+colors = cmr.take_cmap_colors("cmr.tropical", len(bins), cmap_range=(0, 0.85))
+ax[3].bar(bins[:-1], n, width=np.diff(bins), color=colors, edgecolor="black", linewidth=0.5, zorder=4, label=f"Server Bandwidth\nSamples: {len(s_bw4)}")
+ax[3].bar(bins[:-1], n/(bw_4), width=np.diff(bins), color=colors, edgecolor="black", linewidth=0.5, zorder=3, alpha=0.2, label=f"Adjusted for 2400\nServer Samples")
+ax[3].set_title("Beta(10, 5)")
+ax[3].set_xlabel("Bandwidth")
+ax[3].set_ylabel("Frequency")
+ax[3].set_xticks(np.arange(0, 1.1, 0.1))
+
+for a in ax:
+    a.legend()
+plt.suptitle("Server-load Distribution for Different Wire Bandwidth\nDistributions at User Demand of 0.5")
+plt.savefig("./Images/bandwidth-dist.pdf", dpi=500)
 plt.show()
